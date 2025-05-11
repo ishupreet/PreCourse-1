00a096b71b4924b62c9354cf3d9beca203a02183
Ishupreet Singh <isingh@rivian.com>
2025-05-11 15:34:00 -0700

    PreCourse 1

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,46 +1,78 @@
-class Stack { 
+// Time Complexity :
+// push = O(1)
+// pop = O(1)
+// peek  = O(1)
+//
+// Space Complexity :O(N), where N = length of array
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Stack {
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
-    static final int MAX = 1000; 
-    int top; 
-    int a[] = new int[MAX]; // Maximum size of Stack 
-  
-    boolean isEmpty() 
-    { 
-        //Write your code here 
-    } 
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX]; // Maximum size of Stack
 
-    Stack() 
-    { 
-        //Initialize your constructor 
-    } 
-  
-    boolean push(int x) 
-    { 
+    boolean isEmpty()
+    {
+        if( top == -1 ){
+            return true;
+        }
+        return false;
+        //Write your code here
+    }
+
+    Stack()
+    {
+        top = -1;
+        //Initialize your constructor
+    }
+
+    boolean push(int x)
+    {
+        if(top == a.length -1){
+            return false;
+        }
+        a[top+1]=x;
+        top++;
+        return true;
         //Check for stack Overflow
         //Write your code here
-    } 
-  
-    int pop() 
-    { 
+    }
+
+    int pop()
+    {
+        if(top == -1){
+            System.out.println("Stack underflow");
+            return 0;
+        }
+        return a[top--];
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
-    } 
-  
-    int peek() 
-    { 
+    }
+
+    int peek()
+    {
+        if(top == -1){
+            System.out.println("Stack underflow");
+            return -1;
+        }
+        return a[top];
         //Write your code here
-    } 
-} 
-  
-// Driver code 
-class Main { 
-    public static void main(String args[]) 
-    { 
-        Stack s = new Stack(); 
-        s.push(10); 
-        s.push(20); 
-        s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
-    } 
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String args[])
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        System.out.println(s.pop() + " Popped from stack");
+    }
 }
--- Exercise_2.java
+++ Exercise_2.java
@@ -1,52 +1,77 @@
-public class StackAsLinkedList { 
-  
-    StackNode root; 
-  
-    static class StackNode { 
-        int data; 
-        StackNode next; 
-  
-        StackNode(int data) 
-        { 
-            //Constructor here 
-        } 
-    } 
-    
-	
-    public boolean isEmpty() 
-    { 
-        //Write your code here for the condition if stack is empty. 
-    } 
-  
-    public void push(int data) 
-    { 
-        //Write code to push data to the stack. 
-    } 
-  
-    public int pop() 
-    { 	
-	//If Stack Empty Return 0 and print "Stack Underflow"
-        //Write code to pop the topmost element of stack.
-	//Also return the popped element 
-    } 
-  
-    public int peek() 
-    { 
-        //Write code to just return the topmost element without removing it.
-    } 
-  
-	//Driver code
-    public static void main(String[] args) 
-    { 
-  
-        StackAsLinkedList sll = new StackAsLinkedList(); 
-  
-        sll.push(10); 
-        sll.push(20); 
-        sll.push(30); 
-  
-        System.out.println(sll.pop() + " popped from stack"); 
-  
-        System.out.println("Top element is " + sll.peek()); 
-    } 
-} 
+ class StackAsLinkedList {
+
+  StackNode root, top, top_M1;
+
+  static class StackNode {
+
+    int data;
+    StackNode next;
+
+    StackNode(int data) {
+      this.data = data;
+      //Constructor here
+    }
+  }
+
+
+  public boolean isEmpty() {
+    return root == null;
+    //Write your code here for the condition if stack is empty.
+  }
+
+  public void push(int data) {
+    StackNode newNode = new StackNode(data);
+    if (root == null) {
+      root = newNode;
+      top = newNode;
+      return;
+    }
+    newNode.next = top;
+    top = newNode;
+
+    //Write code to push data to the stack.
+  }
+
+  public int pop() {
+    if (root == null) {
+      System.out.println("Stack Underflow");
+      return 0;
+    }
+    int data = top.data;
+    if (root == top) {
+      root = null;
+      top = null;
+      return data;
+    }
+    top =top.next;
+    return data;
+
+    //If Stack Empty Return 0 and print "Stack Underflow"
+    //Write code to pop the topmost element of stack.
+    //Also return the popped element
+  }
+
+  public int peek() {
+    if (root == null) {
+      System.out.println("Stack Underflow");
+      return 0;
+    }
+    return top.data;
+    //Write code to just return the topmost element without removing it.
+  }
+
+  //Driver code
+  public static void main(String[] args) {
+
+    StackAsLinkedList sll = new StackAsLinkedList();
+
+    sll.push(10);
+    sll.push(20);
+    sll.push(30);
+
+    System.out.println(sll.pop() + " popped from stack");
+    sll.push(40);
+    System.out.println("Top element is " + sll.peek());
+    System.out.println(sll.pop() + " popped from stack");
+  }
+}
